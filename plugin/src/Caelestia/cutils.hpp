193494d11855f8b87bfbffffbe9dd8dc9d73252c
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-08-27 17:56:46 +1000

    internal: better copy skipped, I don't want any more dependencies

--- plugin/src/Caelestia/cutils.hpp
+++ plugin/src/Caelestia/cutils.hpp
@@ -9,10 +9,22 @@ class CUtils : public QObject {
     QML_SINGLETON;
 
 public:
+<<<<<<< HEAD
     Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path);
     Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QRect& rect);
     Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, QJSValue onSaved);
     Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, QJSValue onSaved, QJSValue onFailed);
     Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QRect& rect, QJSValue onSaved);
     Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QRect& rect, QJSValue onSaved, QJSValue onFailed);
+=======
+    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path) const;
+    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QRect& rect) const;
+    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, QJSValue onSaved) const;
+    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, QJSValue onSaved, QJSValue onFailed) const;
+    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QRect& rect, QJSValue onSaved) const;
+    Q_INVOKABLE void saveItem(QQuickItem* target, const QUrl& path, const QRect& rect, QJSValue onSaved, QJSValue onFailed) const;
+
+    Q_INVOKABLE bool copyFile(const QUrl& source, const QUrl& target) const;
+    Q_INVOKABLE bool copyFile(const QUrl& source, const QUrl& target, bool overwrite) const;
+>>>>>>> b5d8125 (internal: better copy)
 };
